ba87c830ae704154a026299acfb80a58c6766821
Minh Lê <minhle@192.168.2.2>
2023-07-21 22:51:32 +0700

    first commit

---
+++ Study/Program.cs
@@ -0,0 +1,35 @@
+﻿class animal
+{
+    public string hair;
+    public string color;
+
+    public animal(string haircolor,string colortype)
+    {
+        hair = haircolor;
+        color = colortype;
+    }
+    public animal()
+    {
+        hair = "green";
+        color = "Long";
+    }
+    public void Aype()
+    {
+        Console.WriteLine("Dog: " + hair + " Cat: " + color);
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        animal dog = new animal("blue","red") ;
+        animal cat = new animal();
+
+        dog.Aype();
+        cat.Aype();
+
+
+
+    }
+}
